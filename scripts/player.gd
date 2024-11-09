1b775ada7cc8fabad88ebd8f80d6c6820601921b
potikorn.t <potikorn.t@20scoops.net>
2024-11-09 15:21:36 +0700

    first init

---
+++ scripts/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+signal health_depleted
+
+@export var speed = 600
+@export var health = 100.0
+const DAMAGE_RATE = 5.0
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
+
+	var overlapping_mobs = %HitBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0:
+			health_depleted.emit()
